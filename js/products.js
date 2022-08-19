de2eee3d85053f1297d6ae2c4cb71eaa51024135
jcar512 <jcarballo510@gmail.com>
2022-08-18 23:58:50 -0300

    Agregado login y productos

---
+++ js/products.js
@@ -0,0 +1,34 @@
+var ListaAutos = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    getJSONData(AUTOS).then(resultado => {
+        if (resultado.status === "ok") {
+            ListaAutos = resultado.data.products;
+            console.log(ListaAutos)
+            showCarList()
+        }
+    })
+})
+
+function showCarList() {
+
+    for(let auto of ListaAutos) {
+        document.getElementById("productList").innerHTML +=  `
+            <div onclick="setCatID(${auto.id})" class="list-group-item list-group-item-action cursor-active">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="${auto.image}" alt="${auto.description}" class="img-thumbnail">
+                    </div>
+                    <div class="col">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h4 class="mb-1">${auto.name} - ${auto.currency} ${auto.cost} </h4>
+                            <small class="text-muted">${auto.soldCount} vendidos</small>
+                        </div>
+                        <p class="mb-1">${auto.description}</p>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+}
+
